559e2f8940854fb8fa6b9703bf0b6293c4fb6989
Jake Ryan <jakekryan101@gmail.com>
2023-08-03 03:32:26 -0700

    Improved performance of auto exposure shaders

--- data/shaders/auto_exposure/GenerateLuminanceHistogram.comp.glsl
+++ data/shaders/auto_exposure/GenerateLuminanceHistogram.comp.glsl
@@ -5,11 +5,6 @@
 #include "../Utility.h.glsl"
 #include "AutoExposureCommon.h.glsl"
 
-#define LOCAL_X 16
-#define LOCAL_Y 8
-#define LOCAL_Z 1
-#define WORKGROUPSIZE LOCAL_X * LOCAL_Y * LOCAL_Z
-
 layout(location = 0) uniform sampler2D u_hdrBuffer;
 
 uint colorToBucket(vec3 color)
@@ -21,26 +16,30 @@ uint colorToBucket(vec3 color)
   return clamp(int(mapped), 0, NUM_BUCKETS - 1);
 }
 
-shared int shared_buckets[NUM_BUCKETS];
+shared int sh_buckets[NUM_BUCKETS];
 
-layout (local_size_x = LOCAL_X, local_size_y = LOCAL_Y, local_size_z = LOCAL_Z) in;
+layout (local_size_x = 64, local_size_y = 32, local_size_z = 1) in;
 void main()
 {
-  shared_buckets[gl_LocalInvocationIndex] = 0;
+  const uint localId = gl_LocalInvocationIndex;
+  sh_buckets[localId] = 0;
   
   barrier();
 
-  uvec2 texSize = textureSize(u_hdrBuffer, 0);
-  vec2 upscaleFactor = vec2(texSize) / (gl_NumWorkGroups.xy * gl_WorkGroupSize.xy);
-  uvec2 coords = uvec2(gl_GlobalInvocationID.xy * upscaleFactor);
-  if (!any(greaterThanEqual(coords, texSize)))
+  const uvec2 texSize = textureSize(u_hdrBuffer, 0);
+  const uvec2 coords = uvec2(gl_GlobalInvocationID.xy);
+  if (all(lessThan(coords, texSize)))
   {
     vec3 color = texelFetch(u_hdrBuffer, ivec2(coords), 0).rgb;
     uint bucket = colorToBucket(color);
-    atomicAdd(shared_buckets[bucket], 1);
+    atomicAdd(sh_buckets[bucket], 1);
   }
 
   barrier();
 
-  atomicAdd(autoExposure.histogramBuckets[gl_LocalInvocationIndex], shared_buckets[gl_LocalInvocationIndex]);
+  // Only do one global atomic add per bucket
+  if (localId < NUM_BUCKETS)
+  {
+    atomicAdd(autoExposure.histogramBuckets[localId], sh_buckets[localId]);
+  }
 }
--- data/shaders/auto_exposure/ResolveLuminanceHistogram.comp.glsl
+++ data/shaders/auto_exposure/ResolveLuminanceHistogram.comp.glsl
@@ -1,21 +1,58 @@
 #version 460 core
 #extension GL_GOOGLE_include_directive : enable
+#extension GL_KHR_shader_subgroup_basic : require
+#extension GL_KHR_shader_subgroup_arithmetic : require
 
 #include "../Math.h.glsl"
 #include "AutoExposureCommon.h.glsl"
 
-// TODO: do a multi-lane reduction
-layout (local_size_x = 1, local_size_y = 1, local_size_z = 1) in;
+// gl_SubgroupSize is not a constant, so assume subgroup size is 1 (worst case)
+// Most likely, we will only use 1/32 of this storage
+shared uint sh_scratch[NUM_BUCKETS];
+
+layout (local_size_x = NUM_BUCKETS, local_size_y = 1, local_size_z = 1) in;
 void main()
 {
-  uint sum = 0;
-  for (int i = 0; i < NUM_BUCKETS; i++)
+  const uint i = gl_GlobalInvocationID.x;
+
+  // Weighted
+  const uint firstReduce = subgroupAdd(autoExposure.histogramBuckets[i] * (i + 1));
+
+  // Do a cheeky reduction here to skip an iteration of the loop
+  if (subgroupElect())
   {
-    sum += autoExposure.histogramBuckets[i] * (i + 1);
-    autoExposure.histogramBuckets[i] = 0;
+    sh_scratch[gl_SubgroupID] = firstReduce;
   }
-  
-  float meanLuminance = exp(Remap(float(sum) / float(autoExposure.numPixels), 0.0, NUM_BUCKETS, autoExposure.logMinLuminance, autoExposure.logMaxLuminance));
-  float exposureTarget = autoExposure.targetLuminance / meanLuminance;
-  readExposure = mix(readExposure, exposureTarget, autoExposure.deltaTime * autoExposure.adjustmentSpeed);
+
+  // Reset bucket for next frame
+  autoExposure.histogramBuckets[i] = 0;
+
+  barrier();
+
+  // Most likely, just one iteration of this loop will run.
+  // E.g., 128 buckets / 32 lanes in a subgroup = 4 work items left
+  for (uint work = NUM_BUCKETS / gl_SubgroupSize; work > 0; work /= gl_SubgroupSize)
+  {
+    uint localSum = 0;
+    if (i < work)
+    {
+      localSum = subgroupAdd(sh_scratch[i]);
+    }
+
+    barrier();
+
+    if (subgroupElect())
+    {
+      sh_scratch[gl_SubgroupID] = localSum;
+    }
+
+    barrier();
+  }
+
+  if (gl_GlobalInvocationID.x == 0)
+  {
+    float meanLuminance = exp(Remap(float(sh_scratch[0]) / float(autoExposure.numPixels), 0.0, NUM_BUCKETS, autoExposure.logMinLuminance, autoExposure.logMaxLuminance));
+    float exposureTarget = autoExposure.targetLuminance / meanLuminance;
+    readExposure = mix(readExposure, exposureTarget, autoExposure.deltaTime * autoExposure.adjustmentSpeed);
+  }
 }
